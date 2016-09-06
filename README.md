913aa0bb706a551204d5ac053c53c56aa77f1581
Mingze Zhang <zhangmingze@outlook.com>
2016-09-06 12:51:01 +0800

    tblib added

---
+++ README.md
@@ -0,0 +1,8 @@
+1. Installation (Ubuntu, tested on Ubuntu Windows 10 Bash & Linux Mint 18)
+
+1.1 Build Taobao Libary (tblib)
+
+1.2 Install libraries for OceanBase
+sudo apt-get install liblzo2-dev libsnappy-dev libnuma-dev libaio-dev libgtest-dev libcurl4-openssl-dev libssl-dev libreadline-dev libncurses-dev libmysqlclient-dev
+
+
---
+++ tb-common-utils/build.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+if [ -z $TBLIB_ROOT ]; then
+    echo "TBLIB_ROOT has not set"
+    exit;
+fi
+
+REAL_FILE=`readlink -f $0`
+cd `dirname $REAL_FILE`
+BASE_HOME="`pwd`"
+
+cd $BASE_HOME/tbsys
+if [ -z "$1" -o "$1" = 'make' ]; then  
+	mkdir build
+	cd build
+	cmake ../
+	make -j 8
+fi
+if [ -z "$1" -o "$1" = 'make' -o "$1" = 'install' ]; then 
+	make install
+fi
+
+if [ "$1" = 'clean' ]; then
+	rm build -rf
+fi
+
+cd $BASE_HOME/tbnet 
+if [ -z "$1" -o "$1" = 'make' ]; then  
+	mkdir build
+	cd build
+	cmake ../
+	make -j 8
+fi
+if [ -z "$1" -o "$1" = 'install' ]; then 
+	make install
+fi
+if [ "$1" = 'clean' ]; then
+	rm build -rf
+fi
+
+echo "have installed in $TBLIB_ROOT"
